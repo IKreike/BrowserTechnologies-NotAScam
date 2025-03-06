2e0eb973f091a2f591b33b2024455dcf5c442a70
IKreike <iris.kreike@hva.nl>
2025-03-06 15:26:59 +0100

    6/3/25
    user feedback
    javascript adding required dingen
    
    layout and visual style

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
  Bronnenlijst:
  Form makeup
 https://developer.mozilla.org/en-US/docs/Learn_web_development/Extensions/Forms/How_to_structure_a_web_form
+
+Svg encoder to be able to put it in a content tag
+https://yoksel.github.io/url-encoder/
--- script/script.js
+++ script/script.js
@@ -1,1 +1,30 @@
 console.log("hello world");
+
+// required in het hidden formulier
+
+uitklap = document.querySelector("#uitklap");
+console.log(uitklap);
+
+changeRequired1 = document.querySelector("#MoreForm1");
+changeRequired2 = document.querySelector("#MoreForm2");
+changeRequired3 = document.querySelector("#MoreForm3");
+// console.log(changeRequired);
+
+document.addEventListener("click", AddRequired);
+
+// wanneer word deze functie geroepen? moet ergens voor de submit
+// of kan het tijdens de submit?
+function AddRequired() {
+    if (uitklap.checked == true) {
+        console.log("MoreForm");
+        // add the required tag to uitgeklapt
+        changeRequired1.setAttribute("required", "");
+        changeRequired2.setAttribute("required", "");
+        changeRequired3.setAttribute("required", "");
+    } 
+    else {
+        changeRequired1.removeAttribute("required");
+        changeRequired2.removeAttribute("required");
+        changeRequired3.removeAttribute("required");
+    }
+}
